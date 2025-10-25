e33de32d2ab4cce5d67dd99b043bbf0129948599
Marcal <marcalsantarem@gmail.com>
2025-10-25 01:22:55 -0300

    Criado projeto de backend para o parcelas-app. Spring-boot usando maven e spring initializr

---
+++ parcelas-backend/src/main/java/com/marcal/parcelas_backend/ParcelasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marcal.parcelas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelasBackendApplication.class, args);
+	}
+
+}
---
+++ parcelas-backend/src/test/java/com/marcal/parcelas_backend/ParcelasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcal.parcelas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
