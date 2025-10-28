6c4156c4395a1552a15922c9adff9a849e42c272
Marcal <marcalsantarem@gmail.com>
2025-10-27 23:33:49 -0300

    Criado arquivo import.sql para servir de seed ao banco H2 para testes

---
+++ parcelas-backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_parcela (descricao, valor) VALUES ('PC 1/2', 100.00);
+INSERT INTO tb_parcela (descricao, valor) VALUES ('PC 2/2', 100.00);
+INSERT INTO tb_parcela (descricao, valor) VALUES ('Vinho', 29.90);
+INSERT INTO tb_parcela (descricao, valor) VALUES ('Shopee', 13.50);
